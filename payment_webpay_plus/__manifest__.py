ebbcd0a134fabbea46bcaeb73b9dbd7362d86644
Matias Duran Muñoz <matias.duran.geimser@gmail.com>
2026-07-24 10:13:46 -0400

    Bump WebPay module version for deployment

--- payment_webpay_plus/__manifest__.py
+++ payment_webpay_plus/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Webpay Plus (Transbank) Payment Provider',
-    'version': '19.0.1.0.0',
+    'version': '19.0.1.0.1',
     'category': 'Accounting/Payment Providers',
     'sequence': 350,
     'summary': 'A payment provider for Webpay Plus (Transbank) in Chile',
